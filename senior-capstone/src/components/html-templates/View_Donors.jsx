59e32c4d7d19d02ed4f3bbb4bd59623644d5d3ee
lisaazhaang <lisazhang@oakland.edu>
2022-02-11 00:06:03 -0500

    fixed centering

--- senior-capstone/src/components/html-templates/View_Donors.jsx
+++ senior-capstone/src/components/html-templates/View_Donors.jsx
@@ -6,11 +6,12 @@ export default class View_Donors extends Component {
 <div>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css" />
   <div className="columns">
-    <div className="column is-half is-offset-1">
+    <div className="column is-half is-offset-one-quarter">
       <div className="field is-horizontal">
       </div>
       <div className="field is-horizontal">
       </div>
+      
       <div className="center">
         <h2 className="title is-3">View Matched Donors</h2></div>
       <div className="field is-horizontal" />
@@ -38,6 +39,7 @@ export default class View_Donors extends Component {
           </div> 
         </div>
       </form>
+      <div className="field is-horizontal"> {/* for spacing */}</div>
     </div></div></div>
     );
   }
